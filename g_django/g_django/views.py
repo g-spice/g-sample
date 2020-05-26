89ba5b08dfa618a160121133faa6409dbddd4e7a
g-spice <nao.ca8p@gmail.com>
2020-05-26 15:43:48 +0900

    Thrid Commit

---
+++ g_django/g_django/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from datetime import datetime
+
+def index(request):
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    d = {
+        'hour': datetime.now().hour,
+        'message': 'Sample message',
+    }
+    
+    return render(request, 'home/index.html')
